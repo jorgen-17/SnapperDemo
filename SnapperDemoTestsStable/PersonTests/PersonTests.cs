4c5e5e3ab54c06cf5e51d40894c04fcf160e7827
Jorge Nonell <jorge_nonell@ultimatesoftware.com>
2019-05-15 00:55:54 -0400

    adding project testing v1.4 of snapper

---
+++ SnapperDemoTestsStable/PersonTests/PersonTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Snapper.Json.Nunit;
+using SnapperDemo;
+using Is = Snapper.Json.Nunit.Is;
+
+namespace SnapperDemoTests
+{
+    [TestFixture]
+    public class PersonTests
+    {
+        private const string defaultName = "jorge";
+        private Person person;
+
+        [SetUp]
+        public void Setup()
+        {
+            person = new Person(defaultName);
+        }
+
+        [Test]
+        public void CtorWorks()
+        {
+            Assert.That(person, Is.EqualToSnapshot());
+        }
+
+        [Test]
+        public void GetterAndSetterWork()
+        {
+            person.Name = "luis";
+            Assert.That(person, Is.EqualToSnapshot("update1"));
+
+            person.Name = "nonell";
+            Assert.That(person, Is.EqualToSnapshot("update2"));
+        }
+    }
+}
