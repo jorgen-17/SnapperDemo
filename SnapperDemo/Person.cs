0041adfd100c11be43c8c3b1919adaf873e2cc93
typedefJorge <jnone001@fiu.edu>
2019-05-14 23:03:15 -0400

    init commit

---
+++ SnapperDemo/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace SnapperDemo
+{
+    public class Person
+    {
+        public Person (string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ SnapperDemoTests/PersonTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Snapper;
+using SnapperDemo;
+
+namespace SnapperDemoTests
+{
+    [TestFixture]
+    public class PersonTests
+    {
+        private const string defaultName = "jorge";
+        private Person person;
+
+        [SetUp]
+        public void Setup()
+        {
+            person = new Person(defaultName);
+        }
+
+        [Test]
+        public void CtorWorks()
+        {
+            person.ShouldMatchSnapshot();
+        }
+
+        [Test]
+        public void GetterAndSetterWork()
+        {
+            person.Name = "nonell";
+            person.ShouldMatchSnapshot();
+        }
+    }
+}
